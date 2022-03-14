6f061a2de5a00c7848e47d76f4498b2cd2dfd43f
Menno Geert Eyckens <m.eyckens@student.maastrichtuniversity.nl>
2022-03-14 16:13:53 +0100

    added instance of Ball class

--- core/src/com/angryballs/crazygolf/BallLocationUpdateLoop.java
+++ core/src/com/angryballs/crazygolf/BallLocationUpdateLoop.java
@@ -10,27 +10,19 @@ public class BallLocationUpdateLoop {
     public double Xaccelleration;
     public double Yaccelleration;
     public double heightAccelleration;
+    public Ball bol;
     
-    public BallLocationUpdateLoop(){
-        Xcoordinate = Ball.Xlocation;
-        Ycoordinate = Ball.Ylocation;
-        heightCoordinate = Ball.Zlocation;
-        Xaccelleration = 0.0;
-        Yaccelleration = 0.0;
-        heightAccelleration = 0.0;
+    public BallLocationUpdateLoop(Ball bol){
+        this(bol,0,0,0);
     }
-    public BallLocationUpdateLoop(double x, double y){
-        Xcoordinate = Ball.Xlocation;
-        Ycoordinate = Ball.Ylocation;
-        heightCoordinate = Ball.Zlocation;
-        Xaccelleration = x;
-        Yaccelleration = y;
-        heightAccelleration = 0.0;
+    public BallLocationUpdateLoop(Ball bol, double x, double y){
+        this(bol,x,y,0);
     }
-    public BallLocationUpdateLoop(double x, double y, double height){
-        Xcoordinate = Ball.Xlocation;
-        Ycoordinate = Ball.Ylocation;
-        heightCoordinate = Ball.Zlocation;
+    public BallLocationUpdateLoop(Ball bol, double x, double y, double height){
+        this.bol = bol; 
+        Xcoordinate = bol.Xlocation;
+        Ycoordinate = bol.Ylocation;
+        heightCoordinate = bol.Zlocation;
         Xaccelleration = x;
         Yaccelleration = y;
         heightAccelleration = height;
@@ -54,9 +46,9 @@ public class BallLocationUpdateLoop {
             Xcoordinate += Xvelocity * 0.01;
             Yvelocity += Yvelocity * 0.01;
             heightCoordinate += heightVelocity * 0.01;
-            Ball.Xlocation = Xcoordinate;
-            Ball.Ylocation = Ycoordinate;
-            Ball.Zlocation = heightCoordinate;
+            bol.Xlocation = Xcoordinate;
+            bol.Ylocation = Ycoordinate;
+            bol.Zlocation = heightCoordinate;
         }
     }
 }
