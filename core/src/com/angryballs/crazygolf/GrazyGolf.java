20380791088cd782c903981484d0561cabcf4e94
Antonczegelaar <anton.zegelaar@gmail.com>
2022-03-15 15:21:06 +0100

    Updated golfcourse with a higher resolution

--- core/src/com/angryballs/crazygolf/GrazyGolf.java
+++ core/src/com/angryballs/crazygolf/GrazyGolf.java
@@ -1,31 +1,28 @@
 package com.angryballs.crazygolf;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GrazyGolf extends ApplicationAdapter {
+public class GrazyGolf extends Game {
 	SpriteBatch batch;
-	Texture img;
+	final int SCREENWIDTH = 32;
+	final int SCREENHEIGHT = 32;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new GameScreen(this, 50, 50, SCREENWIDTH, SCREENHEIGHT));
 	}
-
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 1, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
