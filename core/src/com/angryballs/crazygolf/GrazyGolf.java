2f5e63b2ab623861d4916deafe2b869e012fb87c
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-03-16 08:15:25 +0100

    Initial working 3D terrain mesh

--- core/src/com/angryballs/crazygolf/GrazyGolf.java
+++ core/src/com/angryballs/crazygolf/GrazyGolf.java
@@ -1,26 +1,56 @@
 package com.angryballs.crazygolf;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GrazyGolf extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 
+	ModelBatch modelBatch;
+	TerrainModel terMod;
+
+	public PerspectiveCamera cam;
+
+	public Environment environment;
+
 	@Override
 	public void create() {
+		terMod = new TerrainModel(LevelInfo.exampleInput);
+		modelBatch = new ModelBatch();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		cam = new PerspectiveCamera(120, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(0, 20, 0);
+		cam.near = 0;
+		cam.far = 300f;
+		cam.update();
+
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 	}
 
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glDisable(GL20.GL_CULL_FACE);
+		modelBatch.begin(cam);
+		modelBatch.render(terMod.modelInstance);
+		modelBatch.end();
 	}
 
 	@Override
---
+++ core/src/com/angryballs/crazygolf/TerrainModel.java
@@ -0,0 +1,69 @@
+package com.angryballs.crazygolf;
+
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class TerrainModel {
+    private static final int dimension = 256;
+    private static final int divisions = 128;
+    private static final float divSize = dimension / (float) divisions;
+
+    public final ModelInstance modelInstance;
+
+    private final LevelInfo levelInfo;
+
+    public TerrainModel(LevelInfo levelInfo) {
+
+        this.levelInfo = levelInfo;
+
+        var mat = new Material();
+        mat.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("funnyface.jpg")));
+        modelInstance = new ModelInstance(
+                createPlaneModel(10, 10, mat, 0, 0, 1, 1));
+    }
+
+    private Model createPlaneModel(final float width, final float height, final Material material,
+            final float u1, final float v1, final float u2, final float v2) {
+
+        float halfRes = dimension / 2;
+
+        float[][] heightMap = new float[divisions][divisions];
+
+        for (int x = 0; x < divisions; ++x) {
+            for (int y = 0; y < divisions; ++y) {
+                heightMap[x][y] = levelInfo.heightProfile(new Vector2(x * divSize, y * divSize)) * 10;
+            }
+        }
+
+        var modelbuilder = new ModelBuilder();
+
+        modelbuilder.begin();
+        MeshPartBuilder bPartBuilder = modelbuilder.part("rect",
+                GL20.GL_TRIANGLES, Usage.Position | Usage.Normal | Usage.TextureCoordinates,
+                material);
+        // NOTE ON TEXTURE REGION, MAY FILL OTHER REGIONS, USE GET region.getU() and so
+        // on
+        bPartBuilder.setUVRange(u1, v1, u2, v2);
+
+        for (int x = 0; x < divisions - 1; ++x) {
+            for (int y = 0; y < divisions - 1; ++y) {
+                bPartBuilder.rect(
+                        new Vector3(x * divSize - halfRes, heightMap[x][y], y * -divSize + halfRes),
+                        new Vector3((x + 1) * divSize - halfRes, heightMap[x + 1][y], y * -divSize + halfRes),
+                        new Vector3((x + 1) * divSize - halfRes, heightMap[x + 1][y + 1], (y + 1) * -divSize + halfRes),
+                        new Vector3(x * divSize - halfRes, heightMap[x][y + 1], (y + 1) * -divSize + halfRes),
+                        new Vector3(0, 0, 0));
+            }
+        }
+        return (modelbuilder.end());
+    }
+}
