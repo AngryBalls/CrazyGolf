822cae138884aec4071775c6e7c04246a7105bbc
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-03-16 12:54:57 +0100

    Add camera controls

--- core/src/com/angryballs/crazygolf/GrazyGolf.java
+++ core/src/com/angryballs/crazygolf/GrazyGolf.java
@@ -1,7 +1,12 @@
 package com.angryballs.crazygolf;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -11,10 +16,11 @@ import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GrazyGolf extends ApplicationAdapter {
+public class GrazyGolf extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
 
@@ -25,6 +31,8 @@ public class GrazyGolf extends ApplicationAdapter {
 
 	public Environment environment;
 
+	private FirstPersonCameraController camControls;
+
 	@Override
 	public void create() {
 		terMod = new TerrainModel(LevelInfo.exampleInput);
@@ -32,7 +40,7 @@ public class GrazyGolf extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 
-		cam = new PerspectiveCamera(120, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam = new PerspectiveCamera(90, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.set(0, 20, 0);
 		cam.near = 0;
 		cam.far = 300f;
@@ -41,12 +49,21 @@ public class GrazyGolf extends ApplicationAdapter {
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -0f, -1f, -0f));
+
+		Gdx.input.setInputProcessor(this);
+		Gdx.input.setCursorCatched(true);
+
+		camControls = new FirstPersonCameraController(cam);
+		camControls.setDegreesPerPixel(0.5f);
+		camControls.setVelocity(50);
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		camControls.update();
 
 		modelBatch.begin(cam);
 		modelBatch.render(terMod.modelInstance, environment);
@@ -57,5 +74,63 @@ public class GrazyGolf extends ApplicationAdapter {
 	public void dispose() {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch (keycode) {
+			case 51:
+			case 47:
+			case 29:
+			case 32:
+				camControls.keyDown(keycode);
+				break;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		switch (keycode) {
+			case 51:
+			case 47:
+			case 29:
+			case 32:
+				camControls.keyUp(keycode);
+				break;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		camControls.touchDragged(screenX, screenY, 0);
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float amountX, float amountY) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
