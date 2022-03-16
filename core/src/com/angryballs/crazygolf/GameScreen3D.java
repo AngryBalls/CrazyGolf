a2188029114f790504fee399a7f12b93f1be4d23
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-03-16 22:51:29 +0100

    Separate game screen

---
+++ core/src/com/angryballs/crazygolf/GameScreen3D.java
@@ -0,0 +1,93 @@
+package com.angryballs.crazygolf;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+
+public class GameScreen3D extends ScreenAdapter {
+    private final ModelBatch modelBatch = new ModelBatch();
+    private final TerrainModel terrainModel;
+    private PerspectiveCamera cam;
+
+    private final Environment environment;
+
+    private FirstPersonCameraController2 camControls;
+
+    public GameScreen3D(LevelInfo levelInfo) {
+        terrainModel = new TerrainModel(LevelInfo.exampleInput);
+
+        cam = new PerspectiveCamera(90, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(0, 20, 0);
+        cam.near = 1;
+        cam.far = 300f;
+        cam.update();
+
+        camControls = new FirstPersonCameraController2(cam);
+        camControls.setDegreesPerPixel(0.5f);
+        camControls.setVelocity(50);
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -0f, -1f, -0f));
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+
+        camControls.update(delta);
+
+        modelBatch.begin(cam);
+        modelBatch.render(terrainModel.modelInstance, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(new GameScreenInputAdapter());
+        Gdx.input.setCursorCatched(true);
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+        Gdx.input.setCursorCatched(false);
+    }
+
+    @Override
+    public void dispose() {
+        modelBatch.dispose();
+    }
+
+    private class GameScreenInputAdapter extends InputAdapter {
+        @Override
+        public boolean keyDown(int keycode) {
+            camControls.keyDown(keycode);
+            return true;
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            camControls.keyUp(keycode);
+            return true;
+        }
+
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) {
+            camControls.touchDragged(screenX, screenY, 0);
+            return true;
+        }
+    }
+}
