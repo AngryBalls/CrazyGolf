7546ccb48123799fe060481411a3759be1b4b520
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-03-17 16:12:44 +0100

    Add sand pit

--- core/src/com/angryballs/crazygolf/TerrainModel.java
+++ core/src/com/angryballs/crazygolf/TerrainModel.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 public class TerrainModel extends ModelInstance {
-    private static final int dimension = 1024;
+    private static final int dimension = 512;
     private static final int divisions = 128;
     private static final float divSize = dimension / (float) divisions;
 
@@ -24,7 +24,7 @@ public class TerrainModel extends ModelInstance {
 
     private static Model createTerrainModel(LevelInfo levelInfo) {
         var grassMaterial = new Material();
-        grassMaterial.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("ground.png")));
+        grassMaterial.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("grass.png")));
 
         float halfRes = dimension / 2;
 
@@ -35,7 +35,7 @@ public class TerrainModel extends ModelInstance {
                 boolean OOB = x == 0 || x == divisions - 1 || y == 0 || y == divisions - 1;
 
                 heightMap[x][y] = OOB ? -50
-                        : levelInfo.heightProfile(x * divSize, y * divSize).floatValue();
+                        : levelInfo.heightProfile(x * divSize - halfRes, y * divSize - halfRes).floatValue();
             }
         }
 
@@ -51,25 +51,22 @@ public class TerrainModel extends ModelInstance {
 
         for (int x = 0; x < divisions - 1; ++x) {
             for (int y = 0; y < divisions - 1; ++y) {
-                Vector2 uvOffset = isSand(new Vector2(x, y), new Vector2(x + 1, y + 1), levelInfo) ? new Vector2(0, 0)
-                        : new Vector2(0.5f, 0);
-
                 VertexInfo v00 = new VertexInfo().set(
                         new Vector3(x * divSize - halfRes, heightMap[x][y],
                                 y * -divSize + halfRes),
-                        null, null, new Vector2(0, 1f).add(uvOffset));
+                        null, null, new Vector2(0, 1f));
                 VertexInfo v10 = new VertexInfo().set(
                         new Vector3((x + 1) * divSize - halfRes, heightMap[x + 1][y],
                                 y * -divSize + halfRes),
-                        null, null, new Vector2(0.5f, 1).add(uvOffset));
+                        null, null, new Vector2(1, 1));
                 VertexInfo v11 = new VertexInfo().set(
                         new Vector3((x + 1) * divSize - halfRes, heightMap[x + 1][y + 1],
                                 (y + 1) * -divSize + halfRes),
-                        null, null, new Vector2(0.5f, 0).add(uvOffset));
+                        null, null, new Vector2(1, 0));
                 VertexInfo v01 = new VertexInfo().set(
                         new Vector3(x * divSize - halfRes, heightMap[x][y + 1],
                                 (y + 1) * -divSize + halfRes),
-                        null, null, new Vector2(0, 0).add(uvOffset));
+                        null, null, new Vector2(0, 0));
 
                 bPartBuilder.rect(v00, v10, v11, v01);
             }
@@ -98,16 +95,42 @@ public class TerrainModel extends ModelInstance {
                 null, null, new Vector2(0, 0));
 
         bPartBuilder.rect(vw00, vw10, vw11, vw01);
+
+        var sandMaterial = new Material();
+        var sTex = new Texture("sand.png");
+        sTex.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        sandMaterial.set(new TextureAttribute(TextureAttribute.Diffuse, sTex));
+
+        bPartBuilder = modelbuilder.part("srect",
+                GL20.GL_TRIANGLES, Usage.Position | Usage.Normal | Usage.TextureCoordinates,
+                sandMaterial);
+
+        VertexInfo vs00 = new VertexInfo().set(
+                new Vector3(levelInfo.sandPitBounds[0].x,
+                        levelInfo.heightProfile(levelInfo.sandPitBounds[0].x, levelInfo.sandPitBounds[0].y)
+                                .floatValue() + 0.1f,
+                        -levelInfo.sandPitBounds[0].y),
+                null, null, new Vector2(0, 1));
+        VertexInfo vs10 = new VertexInfo().set(
+                new Vector3(levelInfo.sandPitBounds[1].x,
+                        levelInfo.heightProfile(levelInfo.sandPitBounds[1].x, levelInfo.sandPitBounds[0].y)
+                                .floatValue() + 0.1f,
+                        -levelInfo.sandPitBounds[0].y),
+                null, null, new Vector2(1, 1));
+        VertexInfo vs11 = new VertexInfo().set(
+                new Vector3(levelInfo.sandPitBounds[1].x,
+                        levelInfo.heightProfile(levelInfo.sandPitBounds[1].x, levelInfo.sandPitBounds[1].y)
+                                .floatValue() + 0.1f,
+                        -levelInfo.sandPitBounds[1].y),
+                null, null, new Vector2(1, 0));
+        VertexInfo vs01 = new VertexInfo().set(
+                new Vector3(levelInfo.sandPitBounds[0].x,
+                        levelInfo.heightProfile(levelInfo.sandPitBounds[0].x, levelInfo.sandPitBounds[0].y)
+                                .floatValue() + 0.1f,
+                        -levelInfo.sandPitBounds[1].y),
+                null, null, new Vector2(0, 0));
+
+        bPartBuilder.rect(vs00, vs10, vs11, vs01);
         return (modelbuilder.end());
     }
-
-    public static boolean isSand(Vector2 v1, Vector2 v2, LevelInfo info) {
-        var sandBounds = info.sandPitBounds;
-        if (sandBounds.length < 2 || sandBounds.length % 2 != 0)
-            return false;
-
-        return (v1.x >= sandBounds[0].x && v1.y >= sandBounds[0].y && v2.x <= sandBounds[1].x
-                && v2.y <= sandBounds[1].y);
-    }
-
 }
