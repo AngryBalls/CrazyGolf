75d43a15df961c3c2dfba4a9d3596c30c478add0
Ignacio <i.zendera@student.maastrichtuniversity.nl>
2022-03-17 14:15:57 +0100

    DesktopLauncher Changes: - used constant values for the menu screen size (located on the GrazyGolf class) - setResizable to false
    GrazyGolf Changes:
    - Added some constant values for the screen size and the menu size. (Changed the name of the screen size constants)
    
    MenuScreen Changes:
    - Now we have a menu
    - Has constant for the sizes of the button and Logo
    - Images for the buttons and Logo
    - Blue background
    - Buttons "light up" when the mouse is hovered in them
    - Buttons work
    - Logo appears

--- core/src/com/angryballs/crazygolf/MenuScreen.java
+++ core/src/com/angryballs/crazygolf/MenuScreen.java
@@ -12,8 +12,11 @@ public class MenuScreen implements Screen {
     private static final int EXIT_BUTTON_HEIGHT = 100;
     private static final int PLAY_BUTTON_WIDTH = 280;
     private static final int PLAY_BUTTON_HEIGHT = 100;
+    private static final int LOGO_WIDTH = 300;
+    private static final int LOGO_HEIGHT = 150;
     private static final int EXIT_BUTTON_Y = 70;
     private static final int PLAY_BUTTON_Y = 220;
+    private static final int LOGO_Y = 530;
 
     GrazyGolf game;
     OrthographicCamera camera;
@@ -21,6 +24,7 @@ public class MenuScreen implements Screen {
     Texture exitButtonInactive;
     Texture playButtonActive;
     Texture playButtonInactive;
+    Texture logo;
 
     public MenuScreen(GrazyGolf game){
         this.game = game;
@@ -28,6 +32,7 @@ public class MenuScreen implements Screen {
         playButtonInactive = new Texture("play_button_inactive.png");
         exitButtonActive = new Texture("exit_button_active.png");
         exitButtonInactive = new Texture("exit_button_inactive.png");
+        logo = new Texture("logo.png");
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, GrazyGolf.MENU_SCREEN_WIDTH, GrazyGolf.MENU_SCREEN_HEIGHT);
@@ -42,30 +47,32 @@ public class MenuScreen implements Screen {
 
         game.batch.begin();
 
-        //game.font.draw(game.batch, "Welcome to GG!!! ", 100, 150);
-
-        int x = GrazyGolf.MENU_SCREEN_WIDTH/2 - EXIT_BUTTON_WIDTH/2;
+        int x_E = GrazyGolf.MENU_SCREEN_WIDTH/2 - EXIT_BUTTON_WIDTH/2;
+        int x_P =GrazyGolf.MENU_SCREEN_WIDTH/2 - PLAY_BUTTON_WIDTH/2;
+        int x_L =GrazyGolf.MENU_SCREEN_WIDTH/2 - LOGO_WIDTH/2;
+        game.font.draw(game.batch, "Welcome to GG!!! ", 100, 490);
+        game.batch.draw(logo, x_L , LOGO_Y, LOGO_WIDTH, LOGO_HEIGHT);
         /* Changes the color of the buttons when the mouse is on top of the buttons and makes the buttons work
          * */
-        if(Gdx.input.getX() < x + EXIT_BUTTON_WIDTH && Gdx.input.getX() > x && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y){
-            game.batch.draw(exitButtonActive, x, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+        if(Gdx.input.getX() < x_E + EXIT_BUTTON_WIDTH && Gdx.input.getX() > x_E && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y){
+            game.batch.draw(exitButtonActive, x_E, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
             if(Gdx.input.isTouched()){
                 Gdx.app.exit();
             }
         }
         else{
-            game.batch.draw(exitButtonInactive, x, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+            game.batch.draw(exitButtonInactive, x_E, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
         }
 
-        if(Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y){
-            game.batch.draw(playButtonActive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+        if(Gdx.input.getX() < x_P + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x_P && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && GrazyGolf.MENU_SCREEN_HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y){
+            game.batch.draw(playButtonActive, x_P, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
             if (Gdx.input.isTouched()) {
                 game.setScreen(new GameScreen(game, 50, 50, 32, 32));
                 dispose();
             }
         }
         else{
-            game.batch.draw(playButtonInactive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+            game.batch.draw(playButtonInactive, x_P, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
         }
 
         game.batch.end();
