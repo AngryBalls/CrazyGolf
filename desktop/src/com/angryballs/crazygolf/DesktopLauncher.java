abe209931a26501b4b94adf78a40538802815498
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-03-21 08:50:47 +0100

    Attempt to fix rendering on retina configurations

--- desktop/src/com/angryballs/crazygolf/DesktopLauncher.java
+++ desktop/src/com/angryballs/crazygolf/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.angryballs.crazygolf;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
 import com.angryballs.crazygolf.GrazyGolf;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setHdpiMode(HdpiMode.Pixels);
 		new Lwjgl3Application(new GrazyGolf(), config);
 	}
 }
