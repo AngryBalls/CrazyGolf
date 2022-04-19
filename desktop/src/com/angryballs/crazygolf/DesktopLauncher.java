058c8779ab03a122955f1dc5daae06d189eced13
Antonczegelaar <anton.zegelaar@gmail.com>
2022-04-19 11:26:26 +0200

    testing

--- desktop/src/com/angryballs/crazygolf/DesktopLauncher.java
+++ desktop/src/com/angryballs/crazygolf/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setForegroundFPS(10);
 		config.setWindowedMode(GrazyGolf.MENU_SCREEN_WIDTH, GrazyGolf.MENU_SCREEN_HEIGHT);
 		config.setResizable(false);
 		new Lwjgl3Application(new GrazyGolf(), config);
