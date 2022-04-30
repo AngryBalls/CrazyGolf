2444aa085089cc05ebc80563ad70011a57370843
Yahor Dziomin <demin_012@mail.ru>
2022-04-30 23:40:45 +0200

    Added frame for possible SimulatedAnnealing implementation

---
+++ core/src/com/angryballs/crazygolf/AI/SimulatedAnnealing.java
@@ -0,0 +1,38 @@
+package com.angryballs.crazygolf.AI;
+
+import com.angryballs.crazygolf.LevelInfo;
+import com.badlogic.gdx.math.Vector2;
+
+public class SimulatedAnnealing {
+
+    // initial temperature
+    private double temperature = 10000;
+
+    // cooling step size
+    private double coolingRate = 0.005;
+
+    private LevelInfo levelInfo;
+
+    public SimulatedAnnealing(LevelInfo info){
+        this.levelInfo = info;
+    }
+
+    // generates an acceptance possibility for the new distance
+    private double acceptanceChance(double minDist, double curDist, double temperature){
+        // accept better distance in 100%
+        if(minDist>curDist)
+            return 1.0;
+
+        return Math.exp(curDist-minDist)/temperature;
+    }
+
+    public void run(){
+        RuleBasedBot rbb = new RuleBasedBot(levelInfo);
+
+        while (temperature>1){
+
+        }
+    }
+
+
+}
