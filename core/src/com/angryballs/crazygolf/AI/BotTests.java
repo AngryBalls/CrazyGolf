bc723ca26657c4460f03bdba81802031a3acba0b
Yahor Dziomin <demin_012@mail.ru>
2022-05-06 21:15:46 +0200

    Created Newton-Raphson based bot, added explanation

---
+++ core/src/com/angryballs/crazygolf/AI/BotTests.java
@@ -0,0 +1,18 @@
+package com.angryballs.crazygolf.AI;
+
+import com.angryballs.crazygolf.LevelInfo;
+
+public class BotTests {
+    public final static LevelInfo INFO = LevelInfo.exampleInput;
+
+    public static void main(String[] args) {
+//        Bot rbb = new RuleBasedBot(INFO);
+//        rbb.run(rbb);
+        Bot gd = new GradientDescent(INFO);
+        gd.ps.setStateVector(-1,-1,0,0);
+        gd.run(gd);
+        Bot nr = new NewtonRaphson(INFO);
+        nr.ps.setStateVector(-1,-1,0,0);
+        nr.run(nr);
+    }
+}
