d0247d4c7c83dcefc6797af055ede985d5218fdf
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-06-10 17:23:55 +0200

    Working raycasting aim

--- core/src/com/angryballs/crazygolf/Editor/EditorOverlay.java
+++ core/src/com/angryballs/crazygolf/Editor/EditorOverlay.java
@@ -3,6 +3,8 @@ package com.angryballs.crazygolf.Editor;
 import java.util.HashMap;
 
 import com.angryballs.crazygolf.LevelInfo;
+import com.angryballs.crazygolf.Models.BallModel;
+import com.angryballs.crazygolf.Models.TerrainModel;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.math.Intersector;
@@ -20,38 +22,57 @@ public class EditorOverlay {
 
     private LevelInfo levelInfo;
 
+    private TerrainModel gridModel;
+    private BallModel ballModel;
+
     public EditorOverlay(LevelInfo levelInfo) {
         this.levelInfo = levelInfo;
+        gridModel = new TerrainModel(levelInfo, true);
+        gridModel.transform.trn(0, 0.5f, 0);
+
+        ballModel = new BallModel();
+        ballModel.transform.scl(5);
     }
 
     private Vector2 currentlyTargetedNode(Vector3 origin, Vector3 direction) {
-        Vector3 sclDirection = new Vector3(direction).scl(0.01f);
+        Vector3 sclDirection = new Vector3(direction).scl(0.5f);
         Vector3 currentPosition = new Vector3(origin);
 
+        // double lastHeight = currentPosition.y;
+
+        boolean found = false;
         // Find intersect point
         for (int i = 0; i < 100; ++i) {
             currentPosition.add(sclDirection);
 
             var height = levelInfo.heightProfile(currentPosition.x, -currentPosition.z);
-            if (origin.y < height) {
-                if (currentPosition.y >= height)
-                    break;
-            } else if (currentPosition.y <= height)
+            if (currentPosition.y <= height) {
+                found = true;
                 break;
+            }
         }
+        if (!found)
+            return new Vector2();
 
-        float x = Math.round(currentPosition.x);
-        float y = Math.round(-currentPosition.z);
+        float x = Math.round(currentPosition.x / 2) * 2;
+        float y = -Math.round(currentPosition.z / 2) * 2;
 
         return new Vector2(x, y);
     }
 
     public void update(Camera cam) {
+        var pos = currentlyTargetedNode(cam.position, cam.direction);
 
+        var height = levelInfo.heightProfile(pos.x, pos.y);
+
+        var markerPos = new Vector3(pos.x, (float) (height + 0.5), -pos.y);
+
+        ballModel.transform.setTranslation(markerPos);
     }
 
     public void draw(ModelBatch modelBatch) {
-
+        modelBatch.render(gridModel);
+        modelBatch.render(ballModel);
     }
 
     public boolean handleKey(int keycode) {
