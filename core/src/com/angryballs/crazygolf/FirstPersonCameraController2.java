cf605f66bd5bd23dddbef0e7e42f01e800dea849
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-06-12 17:54:22 +0200

    Use fast cam speed only in noclip

--- core/src/com/angryballs/crazygolf/FirstPersonCameraController2.java
+++ core/src/com/angryballs/crazygolf/FirstPersonCameraController2.java
@@ -37,7 +37,7 @@ public class FirstPersonCameraController2 extends InputAdapter {
     private int STRAFE_RIGHT = Keys.D;
     private int FORWARD = Keys.W;
     private int BACKWARD = Keys.S;
-    private float velocity = 50;
+    private float velocity = 5;
     private float degreesPerPixel = 0.5f;
     private final Vector3 tmp = new Vector3();
     private final Vector3 tmp2 = new Vector3();
@@ -104,6 +104,7 @@ public class FirstPersonCameraController2 extends InputAdapter {
     }
 
     public void update(float deltaTime) {
+        var velocity = noclip ? 50 : this.velocity;
         if (keys.containsKey(FORWARD)) {
             tmp.set(camera.direction).nor().scl(deltaTime * velocity);
             camera.position.add(tmp);
